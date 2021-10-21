e865281d0fa44bf316a4ccf7679eb40d50433154
Bogdan <ortbg1988@gmail.com>
2021-10-21 10:58:34 +0300

    [add] add code with lazyload images

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -9,6 +9,7 @@ const images = galleryItems.map(({ preview, original, description }) =>
      `<div class="gallery__item">
        <a class="gallery__link" href="${original}">
         <img
+        loading="lazy"
         class="gallery__image"
         src="${preview}"
         data-source="${original}"
@@ -36,5 +37,26 @@ const onKeyCloseModal = (event) => {
 }
 galleryRef.insertAdjacentHTML("beforeend", images);
 galleryRef.addEventListener('click', onImageOpenModal);
+
+if ('loading' in HTMLImageElement.prototype) {
+    const lazyImages = galleryRef.querySelectorAll('img[loading="lazy"]');
+    lazyImages.forEach(img => 
+        img.src = img.dataset.source
+    );
+ 
+}
+else {
+    const noLazyImage = galleryRef.querySelectorAll('img');
+    noLazyImage.forEach(img => {
+        img.classList.add("lazyload")
+        img.dataset.src = img.dataset.source;
+    });
+    const script = document.createElement('script');
+    script.src = "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js";
+    script.integrity = "sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ==";
+    script.crossOrigin = "anonymous";
+    script.referrerPolicy = "no-referrer"
+    document.body.append(script);
+}
 
 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -8,7 +8,7 @@ const galleryRef = document.querySelector('ul.gallery');
 const images = galleryItems.map(({ preview, original, description }) => 
     `<li class="gallery__item">
       <a class="gallery__link" href="${original}">
-        <img class="gallery__image" src="${preview}" alt="${description}" />
+        <img class="gallery__image" loading="lazy" src="${preview}" alt="${description}" />
       </a>
     </li>`
 ).join('');
@@ -16,3 +16,23 @@ const images = galleryItems.map(({ preview, original, description }) =>
 galleryRef.insertAdjacentHTML("beforeend", images);
 
 let gallery = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250, });
+
+if ('loading' in HTMLImageElement.prototype) {
+    const lazyImages = galleryRef.querySelectorAll('img[loading="lazy"]');
+  lazyImages.forEach(img =>
+    img.dataset.src = img.src
+    );
+}
+else {
+    const noLazyImage = galleryRef.querySelectorAll('img');
+    noLazyImage.forEach(img => {
+        img.classList.add("lazyload")
+        img.dataset.src = img.src;
+    });
+    const script = document.createElement('script');
+    script.src = "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js";
+    script.integrity = "sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ==";
+    script.crossOrigin = "anonymous";
+    script.referrerPolicy = "no-referrer"
+    document.body.append(script);
+}
